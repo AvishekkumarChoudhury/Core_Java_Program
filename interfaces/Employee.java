4a2044ef4a7e520874232b82598cadb7de46fa18
AvishekkumarChoudhury <cavishek56@gmail.com>
2021-03-22 23:58:39 +0530

    ok

---
+++ interfaces/Employee.java
@@ -0,0 +1,34 @@
+public class Employee implements Comparable<Employee> 
+{
+ private String name;
+ private double salary;
+ 
+ public Employee(String name, double salary)
+ {
+     this.name=name;
+     this.salary=salary;
+ }
+  public String getName()
+  {
+      return name;
+  }
+  public double getSalary()
+  {
+      return salary;
+  }
+  public void Salary(double byPercent)
+  {
+      double raise=salary*byPercent/100;
+      salary+=raise;
+  }
+  /**
+   * Compares employees by salary 
+   * @param other anohter Employee object
+   * @return a negative value if this employee has a lower salary than 
+   * otherObject,0 if the salaries are equal,a positive value otherwise
+   */
+  public int compareTo(Employee other)
+{
+    return Double.compare(salary,other.salary);
+}
+}
